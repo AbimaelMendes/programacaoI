d3cda710262131038714da54aefea7a80dc7ce66
abimael.mee@gmail.com <abimael.mee@gmail.com>
2021-03-07 15:54:56 -0300

    exe lista 02

---
+++ lista02.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public static void main(String args[]{
+    Scanner = new Scanner(System.in);
+
+    Autor a = new Autor();
+    a.nome = Scanner.nextLine();
+    a.id = Scanner.nextInt();
+})    
+
+class Autor{
+    String nome;
+    int id;
+
+}
+
+//exe 2
+class Autor{
+    String nome;
+    int id;
+
+    void carregadoteclado(){
+        a.nome = Scanner.nextLine();
+        a.id = Scanner.nextInt();
+    }
+}
